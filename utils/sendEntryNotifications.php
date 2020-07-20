2d17cd3ca3a4ae29392179e373301c9064dffb5f
Dan <79198736000@mail.ru>
2020-07-20 15:39:02 +0300

    Hotfix include_once

--- utils/sendEntryNotifications.php
+++ utils/sendEntryNotifications.php
@@ -1,16 +1,14 @@
 <?php
 
-include_once __DIR__ . "./notificationSender.php";
-include_once __DIR__ . "./../api.php";
+include_once __DIR__ . "/notificationSender.php";
+include_once __DIR__ . "/../api.php";
 
 $MESSAGE = "Как прошел твой день? Сделай об этом запись в дневнике!";
-$INTERVAL = 10;
-$FORMAT = "H:i:00";
 
 $api = new API();
 
 $time = new DateTime("now", new DateTimeZone("UTC"));
-$time = $time->format($FORMAT);
+$time = $time->format("H:i:00");
 
 $query = "SELECT userId FROM entryNotifications WHERE time='$time'";
 
